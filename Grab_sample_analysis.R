2bcc14b1d58a0421d21b9d9ee7394b1ab26f4dee
anna-bergstrom <annabergstrom@boisestate.edu>
2023-01-09 17:54:38 -0700

    added comments and fixed issues with getting FDOM data aggregated to a single dataframe on the same timestep.
    next issue is getting the met and q timeseries data talking to eachother

--- Grab_sample_analysis.R
+++ Grab_sample_analysis.R
@@ -22,8 +22,6 @@ theme_cust <- function(base_size = 16, base_family = "") {
 }
 
 
-
-
 ########### Loading and organizing grab sample data #############
 
 #reading in 2016 & 2017 published data
@@ -317,6 +315,7 @@ incub_names <- unique(incub22$Site)
   
   ################## Loading in FDOM timeseries data ######################
   
+  # loading files and getting the right time zone set 
  FDOM21 <- read.csv('2021.combined.corrected.fDOM.csv')
  FDOM21$Forest.datetime <- mdy_hm(FDOM21$Forest.datetime, tz='America/Anchorage')
  FDOM21$Tundra.datetime <- mdy_hm(FDOM21$Tundra.datetime, tz='America/Anchorage')
@@ -326,31 +325,35 @@ incub_names <- unique(incub22$Site)
  WlFDOM21 <- read.csv('wolv.2021.WT.cor.fDOM.csv')
  WlFDOM21$datetime <- mdy_hm(WlFDOM21$datetime, tz='America/Anchorage')
  
- 
  GlFDOM21 <- read.csv('glacier.2021.wt.cor.fdom.csv')
  GlFDOM21$datetime <- mdy_hm(GlFDOM21$datetime, tz='America/Anchorage')
- glacier <- data.frame((GlFDOM21[,3:4]))
- 
+
+ # pulling out each location into it's own dataframe so it can more easily be used in the next step 
+glacier <- data.frame((GlFDOM21[,3:4]))
 shrub = data.frame((FDOM21[,1:2]))
 forest = data.frame((FDOM21[,3:4]))
 tundra = data.frame((FDOM21[,5:6]))
 nellie = data.frame((FDOM21[,7:8]))
 
-start <- FDOM21$Shrub.datetime[1]
-datetime_target <- data.frame(seq(start, start + days(365), by = "15 min"))
-colnames(datetime_target)<- ('datetime')
+
+start <- FDOM21$Shrub.datetime[1] #finding the first time step with data (i.e. Jan 1 00:00)
+datetime_target <- data.frame(seq(start, start + days(365), by = "15 min")) #making the 15 min timeseries all other data will be matched to. 
+# changing column names in all data frames so they can be merged more easily
+colnames(datetime_target)<- ('datetime') 
 colnames(shrub)<- c('datetime','shrub')
 colnames(forest)<- c('datetime','forest')
 colnames(tundra)<- c('datetime','tundra')
 colnames(nellie)<- c('datetime','nellie')
 
+# Merging all data frames from each site so they all align with a common time step and NA anywhere there is no observation due to sonde being down or different logging interval. 
 FDOM21TS <- merge(datetime_target,shrub, by = 'datetime',all.x = TRUE)
 FDOM21TS <- merge(FDOM21TS,forest, by = 'datetime',all.x = TRUE)
 FDOM21TS <- merge(FDOM21TS,tundra, by = 'datetime',all.x = TRUE)
 FDOM21TS <- merge(FDOM21TS,nellie, by = 'datetime',all.x = TRUE)
 FDOM21TS <- merge(FDOM21TS,glacier, by = 'datetime',all.x = TRUE)
 FDOM21TS <- merge(FDOM21TS,WlFDOM21, by = 'datetime',all.x = TRUE)
-  
+ 
+# repeating all of the above steps for 2022  
  FDOM22 <- read.csv('2022.combined.corrected.fDOM.csv')
  FDOM22$Forest.datetime <- mdy_hm(FDOM22$Forest.datetime, tz='America/Anchorage')
  FDOM22$Tundra.datetime <- mdy_hm(FDOM22$Tundra.datetime, tz='America/Anchorage')
@@ -373,57 +376,99 @@ FDOM21TS <- merge(FDOM21TS,WlFDOM21, by = 'datetime',all.x = TRUE)
  colnames(tundra)<- c('datetime','tundra')
  colnames(nellie)<- c('datetime','nellie')
  
- FDOM22TS <- merge(datetime_target,shrub, by = 'datetime',all.x = TRUE)
- FDOM22TS <- merge(FDOM22TS,forest, by = 'datetime',all.x = TRUE)
+ # forest and Nellie data not started on an even time step starting on may 4 (i.e. 1:06, 1:21, 1:36 ...)
+ #this is just manually adjusting times to bring it back to closest 15 min step (i.e. 1:00, 1:15, 1:30 ...)
+ forest$datetime[forest$datetime > mdy_hms('05/04/2022 12:00:00', tz='America/Anchorage') & forest$datetime < mdy_hms('08/04/2022 11:00:00', tz='America/Anchorage') & !is.na(forest$datetime)] =  
+   forest$datetime[forest$datetime > mdy_hms('05/04/2022 12:00:00', tz='America/Anchorage') & forest$datetime < mdy_hms('08/04/2022 11:00:00', tz='America/Anchorage') & !is.na(forest$datetime)] - minutes(6)
+ 
+ nellie$datetime[ !is.na(nellie$datetime)] =  
+   nellie$datetime[ !is.na(nellie$datetime)] - minutes(9)
+ 
+ FDOM22TS <- merge(datetime_target,forest, by = 'datetime',all.x = TRUE)
+ FDOM22TS <- merge(FDOM22TS,shrub, by = 'datetime',all.x = TRUE)
  FDOM22TS <- merge(FDOM22TS,tundra, by = 'datetime',all.x = TRUE)
  FDOM22TS <- merge(FDOM22TS,nellie, by = 'datetime',all.x = TRUE)
  FDOM22TS <- merge(FDOM22TS,WlFDOM22, by = 'datetime',all.x = TRUE)
  
+ ######### loading in Wolv gage and WX990 data
+ #setting time bounds to pull data
  bounds <- as.POSIXct(c('01/01/2021 00:00:00','10/01/2022 23:45:00'), format="%m/%d/%Y %H:%M:%S", TZ = "America/Anchorage")
  
+ #pulling Q data form NWIS server
  gauge_data <- readNWISdata(sites = '15236900', service = 'iv', parameterCd = '00060', 
                               startDate = as.Date(bounds[1]), endDate = as.Date(bounds[2])) %>%
    select(datetime = dateTime, Q = X_00060_00000) %>%
    mutate(datetime = with_tz(datetime, tz = 'America/Anchorage'),Q_m3s = Q*0.028316847)
- 
+
+ #pulling met data from NWIS server 
 met_data <- readNWISdata(sites = '15236895', service = 'iv', parameterCd = '72194', 
                             startDate = as.Date(bounds[1]), endDate = as.Date(bounds[2]))  %>%
     select(datetime = dateTime, Precip1 = X_..2.._72194_00000, Precip2 = X_72194_00000) %>%
     mutate(datetime = with_tz(datetime, tz = 'America/Anchorage'))
+# met data from server is the raw cumulative time series which includes draining of the TPG
+# the following few lines clumsily deal with that
 
-precip2 <- diff(met_data$Precip2) 
-precip2 <- data.frame(met_data$datetime[2:length(met_data$datetime)],precip2) 
-colnames(precip2)[1] ="datetime" 
-precip2$precip2[precip2$precip2 > 300 |precip2$precip2 < 0 ] = NA
+precip2 <- diff(met_data$Precip2) #taking the difference from one time step to the next to get 15 min instantaneous data 
+precip2 <- data.frame(met_data$datetime[2:length(met_data$datetime)],precip2) #pulling the time stamp along with the 15 min instantaneous data
+colnames(precip2)[1] ="datetime"  #changing the column header
+precip2$precip2[precip2$precip2 > 300 |precip2$precip2 < 0 ] = NA #when the gage was drained there are artificially high values, this turns those and any negative values into no data
 
-  
-Sum <- aggregate(precip2["precip2"], list(hour=cut(as.POSIXct(precip2$datetime), "hour")),sum)
-Sum$precip2[Sum$precip2 == 0 ] = NA 
-colnames(Sum)[1] ="datetime" 
+########################## this is where the issue is ########################
+precip_hourly <- aggregate(precip2["precip2"], list(hour=cut(as.POSIXct(precip2$datetime), "hour")),sum) # summing 15 min data to hourly precip totals
+precip_hourly$precip2[precip_hourly$precip2 == 0 ] = NA  # turning any hour that has no precip to an NA so it doesn't display as a 0 in plots. 
+colnames(precip_hourly)<- c('datetime','precip_mm')
+precip_hourly$datetime <- mdy_hms(precip_hourly$datetime, tz='America/Anchorage')
 
+#merging to one data frame
+start <- precip_hourly$datetime[1] #finding the first time step with data (i.e. Jan 1 00:00)
+datetime_target <- data.frame(seq(start, start + days(730), by = "15 min")) #making the 15 min timeseries all other data will be matched to. 
+# changing column names in all data frames so they can be merged more easily
+colnames(datetime_target)<- ('datetime') 
 
+Precip_q_ts <- merge(datetime_target,gauge_data, by = 'datetime',all.x = TRUE)
+Precip_q_ts <- merge(Precip_q_ts,precip_hourly, by = 'datetime',all.x = TRUE)
+
+# Creating a multi-panel plot of the full two years of data 
+
+#FDOM data 
   ts1<- ggplot()+
-    geom_point(data = FDOM21, aes(x=Forest.datetime, y= Forest), color = "#E2725B", size = 0.2)+
-    geom_point(data = FDOM21, aes(x=Tundra.datetime, y= Tundra), color = "#A80084", size = 0.2 )+
-    geom_point(data = FDOM21, aes(x=Shrub.datetime, y= shrub_creek), color = "#FFAA00", size = 0.2)+
-    geom_point(data = FDOM21, aes(x=Nellie.datetime, y= Nellie_Juan), color = "#EA9DFF", size = 0.2)+
-    geom_point(data = WlFDOM21, aes(x=datetime, y= gage), color = "#73DFFF", size = 0.2)+
-    geom_point(data = GlFDOM21, aes(x=datetime, y= glacier), color = "#0084A8", size = 0.2)+
+    geom_point(data = FDOM21TS, aes(x=datetime, y= forest), color = "#E2725B", size = 0.2)+
+    geom_point(data = FDOM21TS, aes(x=datetime, y= tundra), color = "#A80084", size = 0.2 )+
+    geom_point(data = FDOM21TS, aes(x=datetime, y= shrub), color = "#FFAA00", size = 0.2)+
+    geom_point(data = FDOM21TS, aes(x=datetime, y= nellie), color = "#EA9DFF", size = 0.2)+
+    geom_point(data = FDOM21TS, aes(x=datetime, y= gage), color = "#73DFFF", size = 0.2)+
+    geom_point(data = FDOM21TS, aes(x=datetime, y= glacier), color = "#0084A8", size = 0.2)+
     
-    
-    geom_point(data = FDOM22, aes(x=Forest.datetime, y= Forest), color = "#E2725B", size = 0.2)+
-    geom_point(data = FDOM22, aes(x=Tundra.datetime, y= Tundra), color = "#A80084", size = 0.2 )+
-    geom_point(data = FDOM22, aes(x=Shrub.datetime, y= shrub_creek), color = "#FFAA00", size = 0.2)+
-    geom_point(data = FDOM22, aes(x=Nellie.datetime, y= Nellie_Juan), color = "#EA9DFF", size = 0.2)+
-    geom_point(data = WlFDOM22, aes(x=datetime, y= gage), color = "#73DFFF", size = 0.2)+
+    geom_point(data = FDOM22TS, aes(x=datetime, y= forest), color = "#E2725B", size = 0.2)+
+    geom_point(data = FDOM22TS, aes(x=datetime, y= tundra), color = "#A80084", size = 0.2 )+
+    geom_point(data = FDOM22TS, aes(x=datetime, y= shrub), color = "#FFAA00", size = 0.2)+
+    geom_point(data = FDOM22TS, aes(x=datetime, y= nellie), color = "#EA9DFF", size = 0.2)+
+    geom_point(data = FDOM22TS, aes(x=datetime, y= gage), color = "#73DFFF", size = 0.2)+
     ylab("fDOM (QSU)")+
     xlab('')+
     theme_cust()+
     theme(axis.text = element_text(size = 16))+
     theme(axis.title = element_text(size = 16)) 
   
+  # Calculate the range needed to avoid having your hyetograph and hydrograph overlap 
+  maxRange <- 30 # set how wide of the first axis (streamflow)
+  coeff <- 0.1 # set the shrink coeffcient of Precipitation
+  # Use geom_tile to create the inverted hyetograph
+  # y = the center point of each bar
+  # maxRange - Precipitation/coeff/2
+   ggplot()+
+  #geom_tile(data=precip_hourly, aes(x= datetime, y = maxRange - precip_mm/coeff/2, height = precip_mm/coeff, fill = 'PColor'))+
+    # Plot your discharge data
+    geom_line(data = gauge_data, aes(x= datetime, y = Q_m3s), alpha = 0.8, size = 0.7) +
+    # Create a second axis with sec_axis() and format the labels to display the original precipitation units.
+    scale_y_continuous(name = "Streamflow ()",limit = c(0, maxRange),expand = c(0, 0),sec.axis = sec_axis(trans = ~(.-maxRange)*coeff,name = "Precipitation (mm/hr)"))+
+    scale_fill_manual(values = c('PColor' = "#386cb0"),labels = c('PColor' = 'Precipitation'),name = NULL)+
+    #scale_color_manual(values = c('black', '#e41a1c'), name = NULL)+
+    theme_cust()
+  
+    #Precip and Q data   
  ts2<- ggplot()+
-    geom_point(data = gauge_data, aes(x=datetime, y= Q), color = 'black', size = 0.2)+
+    geom_point(data = gauge_data, aes(x=datetime, y= Q_m3s), color = 'black', size = 0.2)+
     xlim(bounds)+
     ylab(expression(paste("Discharge (m"^"3","s"^"-1", ")")))+
     xlab('')+
