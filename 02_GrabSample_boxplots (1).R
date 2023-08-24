2def7101f5142173a18bf5abb9bdbb0a55a551db
anna-bergstrom <annabergstrom@boisestate.edu>
2023-08-24 13:07:29 -0600

    made some consmetic changes to boxplots

--- 02_GrabSample_boxplots (1).R
+++ 02_GrabSample_boxplots (1).R
@@ -24,9 +24,8 @@ max_threshold <- DOC_table %>%
   filter(doc_detect == TRUE) %>%
   summarise(max = max(DOC))
 
-DOC_plot = ggplot(cendf_DOC, aes(x= factor(group, level = c("Forest" , "Nellie_Juan" , "shrub_creek" , "Tundra" , "lake_inlet", "stream_gauge" ,"Terminus" , "glacier_hut")), y=ros.model, fill= as.factor(group))) +
-  geom_boxplot(coef=1.5
-               , outlier.shape = NA) +
+DOC_plot = ggplot(cendf_DOC, aes(x= factor(group, level = c("Terminus" ,  "stream_gauge" ,"glacier_hut","Nellie_Juan" ,"Tundra" , "lake_inlet","shrub_creek" ,"Forest" )), y=ros.model, fill= as.factor(group))) +
+  geom_boxplot(coef=1.5, outlier.shape = 19) +
   scale_fill_manual(values = c(col.forest, col.nellie, col.shrub, col.tundra, col.gage, col.term, col.glacier, col.lake_in ), breaks = c( "Forest" , "Nellie_Juan" , "shrub_creek" , "Tundra" , "stream_gauge" ,"Terminus" , "glacier_hut", "lake_inlet"))+
   geom_hline(yintercept = max_threshold[[1]], linetype="dashed", color = "#1A237E", size=1) +
   ylab(bquote('DOC' (mgl^-1)))+
@@ -41,8 +40,6 @@ DOC_plot = ggplot(cendf_DOC, aes(x= factor(group, level = c("Forest" , "Nellie_J
 
 print(DOC_plot)
 
-#next need to do a series of pairwise combinations to determine differences, try expand.grid or aggregate 
-
 # Censored data statistics for NO3
 no3_table <- core_sites [!is.na(core_sites$Nitrate),]
 site <- as.factor(no3_table$Site)
@@ -55,9 +52,8 @@ max_threshold <- no3_table %>%
   filter(NO3_detect == TRUE) %>%
   summarise(max = max(Nitrate))
 
-NO3_plot = ggplot(cendf_NO3, aes(x=factor(group, level = c("Forest" , "Nellie_Juan" , "shrub_creek" , "Tundra" , "lake_inlet", "stream_gauge" ,"Terminus" , "glacier_hut")), y=ros.model, fill= as.factor(group))) +
-  geom_boxplot(coef=1.5
-               , outlier.shape = NA) +
+NO3_plot = ggplot(cendf_NO3, aes(x=factor(group, level = c("Terminus" ,  "stream_gauge" ,"glacier_hut","Nellie_Juan" ,"Tundra" , "lake_inlet","shrub_creek" ,"Forest" )), y=ros.model, fill= as.factor(group))) +
+  geom_boxplot(coef=1.5, outlier.shape = 19) +
   scale_fill_manual(values = c(col.forest, col.nellie, col.shrub, col.tundra, col.gage, col.term, col.glacier, col.lake_in ), breaks = c( "Forest" , "Nellie_Juan" , "shrub_creek" , "Tundra" , "stream_gauge" ,"Terminus" , "glacier_hut", "lake_inlet"))+
   geom_hline(yintercept = max_threshold[[1]], linetype="dashed", color = "#1A237E", size=1) +
   ylab(bquote('Nitrate' (mgl^-1)))+
@@ -72,6 +68,34 @@ NO3_plot = ggplot(cendf_NO3, aes(x=factor(group, level = c("Forest" , "Nellie_Ju
 
 print(NO3_plot)
 
+#A series of pairwise combinations to determine differences
+
+# make dataframe for pairwise comparisons, remove duplicates, and create columns to populate with p-values
+site1 <- factor(c("Terminus" ,  "stream_gauge" ,"glacier_hut","Nellie_Juan" ,"Tundra" , "lake_inlet","shrub_creek" ,"Forest" ))
+site2 <- factor(c("Terminus" ,  "stream_gauge" ,"glacier_hut","Nellie_Juan" ,"Tundra" , "lake_inlet","shrub_creek" ,"Forest" ))
+data_comp <- expand.grid(site1,site2) %>%
+  filter(Var1 != Var2) %>%
+  mutate(DOC_p = NaN, DOC_Sig = NaN, NO3_p = NaN, NO3_Sig = NaN) 
+
+for (i in 1:nrow(data_comp)){
+  pairwise <- DOC_table %>%
+    filter(Site == data_comp$Var1[i]|Site==data_comp$Var2[i])
+  P_sites<- as.factor(pairwise$Site)
+  temp <- cendiff(pairwise$DOC, pairwise$doc_detect, P_sites)
+  p.val <- 1 - pchisq(temp$chisq, length(temp$n) - 1)
+  data_comp$DOC_p[i]<-p.val
+  data_comp$DOC_Sig[i]<- p.val<0.05
+  
+  pairwise <- no3_table %>%
+    filter(Site == data_comp$Var1[i]|Site==data_comp$Var2[i])
+  P_sites<- as.factor(pairwise$Site)
+  temp <- cendiff(pairwise$Nitrate, pairwise$NO3_detect, P_sites)
+  p.val <- 1 - pchisq(temp$chisq, length(temp$n) - 1)
+  data_comp$NO3_p[i]<-p.val
+  data_comp$NO3_Sig[i]<- p.val<0.05
+}
+
+data_comp<- data_comp %>% distinct(DOC_p, .keep_all = TRUE)
 
 #Censored data stats for PO4 
 # just basically to see that too many points below detection limit to do much 
@@ -80,7 +104,43 @@ site <- as.factor(po4_table$Site)
 
 cenfit(po4_table$Phosphate_P, po4_table$PO4_detect, site)
 
+# Flourescence Index boxplot
+ggplot(core_sites, aes(x=reorder(Site,DOC,na.rm = TRUE), y= FI, fill= as.factor(Site))) +
+  geom_boxplot(outlier.shape =  19) +
+  scale_fill_manual(values = c(col.forest, col.nellie, col.shrub, col.tundra, col.gage, col.term, col.glacier, col.lake_in  ), breaks = c( "Forest" , "Nellie_Juan" , "shrub_creek" , "Tundra" , "stream_gauge" ,"Terminus" , "glacier_hut", "lake_inlet"))+
+  #geom_jitter(shape=16, position=position_jitter(0.2))+
+  geom_hline(yintercept=1.9, linetype="dashed", color = "#000000", size=1)+
+  geom_hline(yintercept=1.4, linetype="dashed", color = "#999999", size=1)+
+  ylab("Fluorescence Index")+
+  xlab("")+
+  scale_x_discrete(labels=c("Forest" = "Forest", "Nellie_Juan" = "Nellie Juan" , "shrub_creek"= "Shrub" , "Tundra"= "Tundra" , "stream_gauge"= "Gage" ,"Terminus" =  "Terminus", "glacier_hut" = "Glacier", "lake_inlet" = "Upper Tundra"))+
+  theme_cust() +
+  theme(axis.text.x=element_text(angle = -45, hjust = 0))+
+  theme(legend.position = "none")+ 
+  theme(aspect.ratio = 1/1)+
+  theme(axis.text = element_text(size = 16))+
+  theme(axis.title = element_text(size = 16))
 
+# Phosphate boxplot 
+ggplot(core_sites, aes(x=reorder(Site,DOC,na.rm = TRUE), y= Phosphate_P, color= as.factor(Site))) +
+  scale_color_manual( values = c("#E2725B", "#EA9DFF", "#FFAA00", "#A80084", "#73DFFF", "#059E41", "#0084A8", "#6600CC" ), breaks = c( "Forest" , "Nellie_Juan" , "shrub_creek" , "Tundra" , "stream_gauge" ,"Terminus" , "glacier_hut", "lake_inlet"))+
+  #geom_boxplot(outlier.shape =  NA) +
+  geom_jitter(shape=16, position=position_jitter(0.1))+
+  geom_hline(yintercept = 0.03, linetype="dashed", color = "#1A237E", size=1) +
+  ylab(bquote('Phosphate' (mgl^-1)))+
+  xlab("")+
+  ylim(0,0.06)+
+  scale_x_discrete(labels=c("Forest" = "Forest", "Nellie_Juan" = "Nellie Juan" , "shrub_creek"= "Shrub" , "Tundra"= "Tundra" , "stream_gauge"= "Gage" ,"Terminus" =  "Terminus", "glacier_hut" = "Glacier", "lake_inlet" = "Upper Tundra"))+
+  theme_cust() +
+  theme(axis.text.x=element_text(angle = -45, hjust = 0))+
+  theme(legend.position = "none")+
+  theme(aspect.ratio = 1/1)+
+  theme(axis.text = element_text(size = 16))+
+  theme(axis.title = element_text(size = 16))
+
+
+
+########## No longer used ##############
 # DOC boxplot
 ggplot(core_sites, aes(x=reorder(Site,DOC,na.rm = TRUE), y= DOC, color= as.factor(Site))) +
   scale_color_manual( values = c(col.forest, col.nellie, col.shrub, col.tundra, col.gage, col.term, col.glacier, col.lake_in ), breaks = c( "Forest" , "Nellie_Juan" , "shrub_creek" , "Tundra" , "stream_gauge" ,"Terminus" , "glacier_hut", "lake_inlet"))+
@@ -96,22 +156,6 @@ ggplot(core_sites, aes(x=reorder(Site,DOC,na.rm = TRUE), y= DOC, color= as.facto
   theme(axis.text = element_text(size = 16))+
   theme(axis.title = element_text(size = 16))  
 
-# Flourescence Index boxplot
-ggplot(core_sites, aes(x=reorder(Site,DOC,na.rm = TRUE), y= FI, color= as.factor(Site))) +
-  scale_color_manual(values = c(col.forest, col.nellie, col.shrub, col.tundra, col.gage, col.term, col.glacier, col.lake_in  ), breaks = c( "Forest" , "Nellie_Juan" , "shrub_creek" , "Tundra" , "stream_gauge" ,"Terminus" , "glacier_hut", "lake_inlet"))+
-  geom_boxplot(outlier.shape =  NA) +
-  geom_jitter(shape=16, position=position_jitter(0.2))+
-  geom_hline(yintercept=1.9, linetype="dashed", color = "#1A237E", size=1)+
-  geom_hline(yintercept=1.4, linetype="dashed", color = "#43A047", size=1)+
-  ylab("Fluorescence Index")+
-  xlab("")+
-  scale_x_discrete(labels=c("Forest" = "Forest", "Nellie_Juan" = "Nellie Juan" , "shrub_creek"= "Shrub" , "Tundra"= "Tundra" , "stream_gauge"= "Gage" ,"Terminus" =  "Terminus", "glacier_hut" = "Glacier", "lake_inlet" = "Upper Tundra"))+
-  theme_cust() +
-  theme(axis.text.x=element_text(angle = -45, hjust = 0))+
-  theme(legend.position = "none")+ 
-  theme(aspect.ratio = 1/1)+
-  theme(axis.text = element_text(size = 16))+
-  theme(axis.title = element_text(size = 16))
 
 # Nitrate boxplot
 ggplot(core_sites, aes(x=reorder(Site,DOC,na.rm = TRUE), y= Nitrate, color= as.factor(Site))) +
@@ -128,19 +172,4 @@ ggplot(core_sites, aes(x=reorder(Site,DOC,na.rm = TRUE), y= Nitrate, color= as.f
   theme(axis.text = element_text(size = 16))+
   theme(axis.title = element_text(size = 16))
 
-# Phosphate boxplot 
-ggplot(core_sites, aes(x=reorder(Site,DOC,na.rm = TRUE), y= Phosphate_P, color= as.factor(Site))) +
-  scale_color_manual( values = c("#E2725B", "#EA9DFF", "#FFAA00", "#A80084", "#73DFFF", "#059E41", "#0084A8", "#6600CC" ), breaks = c( "Forest" , "Nellie_Juan" , "shrub_creek" , "Tundra" , "stream_gauge" ,"Terminus" , "glacier_hut", "lake_inlet"))+
-  geom_boxplot(outlier.shape =  NA) +
-  geom_jitter(shape=16, position=position_jitter(0.2))+
-  ylab(bquote('Phosphate' (mgl^-1)))+
-  xlab("")+
-  ylim(0,0.1)+
-  scale_x_discrete(labels=c("Forest" = "Forest", "Nellie_Juan" = "Nellie Juan" , "shrub_creek"= "Shrub" , "Tundra"= "Tundra" , "stream_gauge"= "Gage" ,"Terminus" =  "Terminus", "glacier_hut" = "Glacier", "lake_inlet" = "Upper Tundra"))+
-  theme_cust() +
-  theme(axis.text.x=element_text(angle = -45, hjust = 0))+
-  theme(legend.position = "none")+
-  theme(aspect.ratio = 1/1)+
-  theme(axis.text = element_text(size = 16))+
-  theme(axis.title = element_text(size = 16))
 
